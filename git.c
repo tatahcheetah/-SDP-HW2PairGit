016e4c08e7f0fce6899eb69d49563c0127e118b5
Navaphon <kr.navaphon_st@tni.ac.th>
2016-12-16 04:09:17 +0000

    fourth commit

--- git.c
+++ git.c
@@ -4,13 +4,37 @@
 int main()
 {
     srand(time(NULL));
+    int i,j;
+    
+    for(i=0;i<10;i++)
+    {
+        printf("%d\n",i);
+    }
+    printf("\n");
+    
+    printf("Lotto is ");
+    for(i=0;i<6;i++)
+    {
+        printf("%d", rand() % 10);
+    }
+    
+    printf("\n\n");
 
-    for(int i=0;i<4;i++)
+    for(i=0;i<4;i++)
     {
         printf("Last 3 Digi Lotto ");
-        for(int j=0;j<3;j++)
+        for(j=0;j<3;j++)
             printf("%d", rand() % 10);
         printf("\n");
     }
+    
+    printf("\n");
+    
+    
+    printf("Last 2 Digi Lotto ");
+    for(j=0;j<2;j++)
+        printf("%d", rand() % 10);
+    printf("\n");
+    
     
 }
